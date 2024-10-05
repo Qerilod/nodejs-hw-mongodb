f2c6beef0c1cd4bf6f1f6728a2d39c96c93c14c0
Qerilod <140080894+Qerilod@users.noreply.github.com>
2024-10-05 20:13:36 +0300

    comm-3

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -3,14 +3,18 @@ import Joi from 'joi';
 export const postSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
   email: Joi.string().email(),
-  phone: Joi.string().min(3).max(20).required(),
+  phoneNumber: Joi.string().min(3).max(20).required(),
+  contactType: Joi.string().valid('personal', 'work', 'home'),
+  isFavourite: Joi.boolean(),
 });
 
 export const patchSchema = Joi.object({
   name: Joi.string().min(3).max(20),
   email: Joi.string().email(),
-  phone: Joi.string().min(3).max(20),
-}).min(1);
+  phoneNumber: Joi.string().min(3).max(20),
+  contactType: Joi.string().valid('personal', 'work', 'home'),
+  isFavourite: Joi.boolean(),
+});
 
 export const validateBody = (schema) => {
   return (req, res, next) => {
